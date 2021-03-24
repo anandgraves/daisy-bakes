cad4143e1927ec5dd76f6ceca3dd931e3d61aa03
Anand Graves <anandgraves@users.noreply.github.com>
2021-03-24 20:51:44 +0100

    feat: add feature-v12 component

---
+++ components/feature-v12/feature-v12.vue
@@ -0,0 +1,35 @@
+<template>
+  <section
+    class="feature-v12 feature-v12--top feature-v12--left@md feature-v12--offset-sm feature-v12--offset-xl@md padding-top-xl padding-bottom-xxl"
+  >
+    <div class="container max-width-adaptive-lg">
+      <div class="grid flex-center">
+        <div class="col-10 col-5@md">
+          <div class="feature-v12__offset-item text-center text-left@md">
+            <h1 class="text-xxxl@sm">
+              Daisy Bakes maakt voor jou Homemade Feel-Good Lekkernijen.
+            </h1>
+            <div class="margin-top-sm">
+              <div class="flex flex-wrap gap-sm items-center margin-x-auto">
+                <a href="#0" class="btn btn--primary">Neem Een Kijkje</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-7@md">
+          <figure class="feature-v12__bg-item">
+            <img
+              class="block width-100%"
+              src="/images/friandises.jpg"
+              alt="Image description"
+            />
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+@import 'feature-v12';
+</style>
