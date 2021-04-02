b76696f389a348e0b1b476657b6d21889f825be5
Anand Graves <anandgraves@users.noreply.github.com>
2021-04-02 12:43:59 +0200

    feat: add newsletter signup page

---
+++ pages/nieuwsbrief.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="newsletter-page padding-top-lg padding-bottom-xxl">
+    <div class="container max-width-xs">
+      <h1 class="text-center">Verbeter jouw Kookvaardigheden</h1>
+      <form class="margin-top-md">
+        <div class="text-component text-center">
+          <p class="margin-bottom-xs">
+            Ik houd je op de hoogte van kortingen, tips en recepten. <br />
+            Schrijf je in en ontvang <strong>nu 10% korting</strong> op je
+            eerste aankoop.
+          </p>
+        </div>
+        <newsletter-input class="margin-top-md" />
+      </form>
+    </div>
+  </article>
+</template>
+
+<style lang="scss">
+.newsletter-page {
+  background-color: var(--color-accent);
+}
+</style>
