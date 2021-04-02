6fb2a8432c6703dee8196e0a682b654b0aa6dc2a
Anand Graves <anandgraves@users.noreply.github.com>
2021-04-02 14:02:59 +0200

    feat: add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="newsletter-page padding-top-lg padding-bottom-xxl">
+    <div class="container max-width-xs text-component">
+      <h1 class="text-center">Contact</h1>
+      <p class="margin-top-lg">
+        Lekkers van DaisyBakes.nl is af te halen in Almere of kan tegen een
+        kleine vergoeding bezorgd worden in omgeving Almere/Amsterdam.
+      </p>
+      <p>
+        Heb je interesse of wil je meer informatie? Stuur mij dan een berichtje
+        op <a href="mailto:info@daisybakes.nl">info@daisybakes.nl</a> en ik help
+        je graag verder!
+      </p>
+
+      <p>
+        Je kunt mij ook bereiken via
+        <a href="https://instagram.com/daisybakes.nl">Instagram</a>.
+      </p>
+    </div>
+  </article>
+</template>
+
+<style lang="scss">
+.newsletter-page {
+  background-color: var(--color-accent);
+}
+</style>
